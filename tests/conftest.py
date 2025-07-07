d13f300b8e138ecd4a8938261c1032224163bd85
Filip Mivalt <mivalt.filip@mayo.edu>
2025-07-07 09:56:41 -0500

    Fmivalt/dev (#1) (#3)
    * github workflows
    
    * bci2000_to_mefd

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,16 +6,24 @@ import time
 
 import pytest
 
+@pytest.fixture
+def tmp_dir():
+    tmpdir = tempfile.mkdtemp()
+
+    yield tmpdir
+
+    shutil.rmtree(tmpdir)
+    while os.path.exists(tmpdir):
+        time.sleep(0.1)
+
 
 @pytest.fixture
-def test_file_bci2000_stim():
+def test_file_bci2000_stim(tmp_dir):
     path_bci2000 = os.path.abspath(__file__)
     path_bci2000 = path_bci2000.split(os.sep)[:-1] + ['files', 'test_stim_file.dat'] # Navigate to the parent directory of the test file
     path_bci2000 = os.sep.join(path_bci2000)
 
-    tmpdir = tempfile.mkdtemp()
-
-    tmp_bci2000 = os.path.join(tmpdir, 'test_stim_file.dat')
+    tmp_bci2000 = os.path.join(tmp_dir, 'test_stim_file.dat')
     shutil.copyfile(path_bci2000, tmp_bci2000)
 
     orig_file_size = os.path.getsize(path_bci2000)
@@ -25,20 +33,15 @@ def test_file_bci2000_stim():
         if os.path.exists(tmp_bci2000):
             new_file_size = os.path.getsize(tmp_bci2000)
 
-
-    yield tmp_bci2000, tmpdir
-
-    shutil.rmtree(tmpdir)
+    yield tmp_bci2000, tmp_dir
 
 @pytest.fixture
-def test_file_bci2000_accl():
+def test_file_bci2000_accl(tmp_dir):
     path_bci2000 = os.path.abspath(__file__)
     path_bci2000 = path_bci2000.split(os.sep)[:-1] + ['files', 'test_accl_file.dat'] # Navigate to the parent directory of the test file
     path_bci2000 = os.sep.join(path_bci2000)
 
-    tmpdir = tempfile.mkdtemp()
-
-    tmp_bci2000 = os.path.join(tmpdir, 'test_accl_file.dat')
+    tmp_bci2000 = os.path.join(tmp_dir, 'test_accl_file.dat')
     shutil.copyfile(path_bci2000, tmp_bci2000)
 
     orig_file_size = os.path.getsize(path_bci2000)
@@ -48,10 +51,7 @@ def test_file_bci2000_accl():
         if os.path.exists(tmp_bci2000):
             new_file_size = os.path.getsize(tmp_bci2000)
 
-
-    yield tmp_bci2000, tmpdir
-
-    shutil.rmtree(tmpdir)
+    yield tmp_bci2000, tmp_dir
 
 
 
